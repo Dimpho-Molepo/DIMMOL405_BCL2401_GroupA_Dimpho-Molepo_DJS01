9ff0301c56ea429dcab7b9c57c98a27267b673e5
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-13 12:58:46 +0200

    Fixed some indenting

--- Mars Climate Orbiter Challenge/index.js
+++ Mars Climate Orbiter Challenge/index.js
@@ -4,35 +4,35 @@ import newDistanceCalculation from "./newDistanceCalculation.js";
 
 // Props of the given parameters
 const props = {
-    velocity: {
-        velocityValue: 10000,
-        velocityUnits: "km/h",
-      },
-      acceleration: {
-        accelerationValue: 3,
-        accelerationUnits: "m/s^2",
-      },
-      distance: {
-        distanceValue: 0,
-        distanceUnits: "km",
-      },
-      time: {
-        timeValue: 3600,
-        timeUnits: "s",
-      },
-      fuel: {
-        fuelValue: 5000,
-        fuelUnits: "kg",
-      },
-      fuelBurnRate: {
-        fuelBurnRateValue: 0.5,
-        fuelBurnRateUnits: "kg/s",
-      }
-}
+  velocity: {
+    velocityValue: 10000,
+    velocityUnits: "km/h",
+  },
+  acceleration: {
+    accelerationValue: 3,
+    accelerationUnits: "m/s^2",
+  },
+  distance: {
+    distanceValue: 0,
+    distanceUnits: "km",
+  },
+  time: {
+    timeValue: 3600,
+    timeUnits: "s",
+  },
+  fuel: {
+    fuelValue: 5000,
+    fuelUnits: "kg",
+  },
+  fuelBurnRate: {
+    fuelBurnRateValue: 0.5,
+    fuelBurnRateUnits: "kg/s",
+  },
+};
 
 const newVelocity = calculateNewVelocity(props); //calculates new velocity based on acceleration and time
 const newDistance = newDistanceCalculation(props); //calculates new distance based on velocity and time trravelled
-const remainingFuel = remainingFuelCalculation(props) //calculates fuel used based on distance travelled and the fuel burn rate
+const remainingFuel = remainingFuelCalculation(props); //calculates fuel used based on distance travelled and the fuel burn rate
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
